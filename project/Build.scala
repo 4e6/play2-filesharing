41f220c8a7ee89acfa30989a49e52a2bc59f8ae6
Dmitriy Bushev <bushevdv@gmail.com>
2012-01-22 19:04:22 +0400

    Added Scalate Dependency

--- project/Build.scala
+++ project/Build.scala
@@ -4,8 +4,18 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
   val appName = "Web Clipboard"
+
   val appVersion = "0.1"
-  val appDependencies = Nil
+
+  val appDependencies = Seq(
+    "org.fusesource.scalate" % "scalate-core" % "1.5.3")
+
+  val appResolvers = Seq(
+    DefaultMavenRepository,
+    "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+    Resolver.file("Local Play20 Repository", file("/opt/git-repos/play20/repository/local"))(Resolver.ivyStylePatterns))
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA)
+    .settings(
+      resolvers ++= appResolvers)
 }
