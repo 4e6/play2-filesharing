747ee9d67dd72b58d3e16c4fd5eca579d3750c14
Dmitriy Bushev <bushevdv@gmail.com>
2012-01-24 21:30:12 +0400

    Added default imports to all Scalate templates

---
+++ app/scalate/ScalatePackage.scala
@@ -0,0 +1,16 @@
+import org.fusesource.scalate.{TemplateSource, Binding}
+import org.fusesource.scalate.support.TemplatePackage
+
+/** Standart imports for Play templates */
+class ScalatePackage extends TemplatePackage {
+  override def header(source: TemplateSource, bindings: List[Binding]) =
+    """
+    | import play.api.templates._
+    | import play.api.templates.PlayMagic._
+    | import models._
+    | import controllers._
+    | import play.api.i18n.Messages
+    | import play.api.mvc._
+    | import play.api.data._
+    """.stripMargin
+}
