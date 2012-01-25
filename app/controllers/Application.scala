9581d8cb7e8a88f6b081717bdbb9319bf1ede158
Dmitriy Bushev <bushevdv@gmail.com>
2012-01-26 01:40:27 +0400

    Added saving file to database basic feature

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,10 +2,14 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.mvc.MultipartFormData._
 import play.api.templates.HtmlFormat
 
 import org.fusesource.scalate._
 import org.squeryl.PrimitiveTypeMode._
+import scalax.io.Resource
+import java.sql.Timestamp
+
 import models.File
 import models.Files._
 
@@ -21,10 +25,33 @@ object Application extends Controller {
   }
 
   def uploadFile = Action(parse.multipartFormData) { request =>
-    val rawFile = request.body.files
-    //transaction {
-    //files.insert(new File(f))
-    //}
-    Ok("File saved as ")
+    Logger.info("Start file uploading...")
+    Logger.info("body[" + request.body + "]")
+
+    val password = request.body.dataParts.get("password") flatMap { _.headOption }
+
+    val uploadedFile = request.body.files.headOption map { filepart =>
+      val FilePart(_, filename, _, ref) = filepart
+      (filename, ref.file)
+    }
+
+    if (uploadedFile.isDefined) {
+      val (filename, file) = uploadedFile.get
+      val url = "http://localhost/" + filename
+      transaction {
+        files insert {
+          new File(url,
+                   filename,
+                   Resource.fromFile(file).byteArray,
+                   new Timestamp(System.currentTimeMillis),
+                   new Timestamp(System.currentTimeMillis + 60*60*1000),
+                   password,
+                   None,
+                   None)
+        }
+      }
+      Ok("File saved as " + uploadedFile.get._1)
+    } else
+      Ok("File upload failed")
   }
 }
