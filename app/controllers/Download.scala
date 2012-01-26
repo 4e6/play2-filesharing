d72664229095b736f932772c7de4e47e0a51dc7c
Dmitriy Bushev <bushevdv@gmail.com>
2012-01-26 20:41:13 +0400

    Request handling refactored with Scalaz

--- app/controllers/Download.scala
+++ app/controllers/Download.scala
@@ -1,12 +1,24 @@
 package controllers
 
+import scalaz._
+import Scalaz._
+
 import play.api._
 import play.api.mvc._
+
+import org.squeryl.PrimitiveTypeMode._
+import models.File
+import models.Files._
 
 trait Download {
   self: Controller =>
 
   def downloadFile(url: String) = Action {
-    Ok("got [" + url + "]")
+    transaction {
+      val file = files.where(f => f.url === url).headOption
+      file.cata(
+        s => Ok("Got [" + s.url + "]"),
+        Ok("File [" + url + "] not found"))
+    }
   }
 }
--- app/controllers/Upload.scala
+++ app/controllers/Upload.scala
@@ -1,5 +1,8 @@
 package controllers
 
+import scalaz.{ Logger => SLogger, _ }
+import Scalaz._
+
 import play.api._
 import play.api.mvc._
 import play.api.mvc.MultipartFormData._
@@ -31,24 +34,28 @@ trait Upload {
       (filename, ref.file)
     }
 
-    if (uploadedFile.isDefined) {
-      val (filename, file) = uploadedFile.get
-      val url = "http://localhost/" + filename
-      transaction {
-        files insert {
-          new File(
-            url,
-            filename,
-            Resource.fromFile(file).byteArray,
-            new Timestamp(timeNow),
-            new Timestamp(timeToStore),
-            password,
-            None,
-            None)
-        }
-      }
-      Ok("File saved as " + filename)
-    } else
-      Ok("File upload failed")
+    uploadedFile.cata(f => success("", f, timeNow, timeToStore, password, None, None), failure)
   }
+
+  def success(url: String,
+              nameFile: (String, java.io.File),
+              from: Long,
+              to: Long,
+              pass: Option[Array[Byte]],
+              q: Option[String],
+              a: Option[Array[Byte]]) = {
+    val (name, file) = nameFile
+    transaction {
+      files insert new File(
+        url,
+        name,
+        Resource.fromFile(file).byteArray,
+        new Timestamp(from),
+        new Timestamp(to),
+        pass, q, a)
+    }
+    Ok("File saved as " + name)
+  }
+
+  def failure = Ok("File upload failed")
 }
