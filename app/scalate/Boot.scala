4be93464961cb2a05378b3e510a758677564e8c4
Dmitriy Bushev <bushevdv@gmail.com>
2012-01-27 12:12:38 +0400

    Set Scalate html format to html5

--- app/scalate/Boot.scala
+++ app/scalate/Boot.scala
@@ -3,6 +3,7 @@ package scalate
 import play.api._
 import org.fusesource.scalate.TemplateEngine
 import org.fusesource.scalate.layout.DefaultLayoutStrategy
+import org.fusesource.scalate.scaml.ScamlOptions
 import java.io.File
 
 class Boot(engine: TemplateEngine) {
@@ -12,6 +13,7 @@ class Boot(engine: TemplateEngine) {
     engine.workingDirectory = new File(path, "/target/scala-2.9.1")
     engine.sourceDirectories = Seq(new File(path, "/app"))
     engine.layoutStrategy = new DefaultLayoutStrategy(engine)
+    ScamlOptions.format = ScamlOptions.Format.html5
     Logger.info("Scalate configured")
   }
 }
