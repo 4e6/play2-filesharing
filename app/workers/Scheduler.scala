07e364d9884fa5f88ef6bb2d5a1d5829b953c814
Dmitriy Bushev <bushevdv@gmail.com>
2012-03-29 13:05:07 +0400

    Further optimisation of scheduled task

--- app/workers/Scheduler.scala
+++ app/workers/Scheduler.scala
@@ -13,7 +13,6 @@ object Scheduler {
   def job() {
     import models._
     import controllers.Helpers._
-    import scalax.file.Path
     import org.squeryl.PrimitiveTypeMode._
     Logger.info("Running scheduled job")
 
@@ -26,22 +25,19 @@ object Scheduler {
       }
     }
 
-    if (transaction(urls.nonEmpty)) {
-      val deletedUrls = transaction {
-        urls filter { url =>
-          val dir = Storage.root / url
-          assert(dir.parent.get == Storage.root)
-          Logger.info("Deleting: " + dir)
-          val (deleted, remains) = dir.deleteRecursively()
-          Logger.info("Deletion stats[" + deleted + "," + remains + "]")
-          remains == 0
-        }
+    val deletedUrls = transaction {
+      urls filter { url =>
+        val dir = Storage.root / url
+        val (deleted, remains) = dir.deleteRecursively(continueOnFailure = true)
+        val msg = "Deleting " + dir + " [" + deleted + "," + remains + "]"
+        if (deleted == 2) Logger.debug(msg) else Logger.error(msg)
+        remains == 0
       }
-
-      transaction(Storage.files deleteWhere { f => f.url in deletedUrls })
-      transaction(Storage.schedule deleteWhere { t => t.url in deletedUrls })
-
     }
+
+    transaction(Storage.files deleteWhere { _.url in deletedUrls })
+    transaction(Storage.schedule deleteWhere { _.url in deletedUrls })
+
   }
 
   def start() {
