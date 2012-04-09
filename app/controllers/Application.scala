953794916f1721fbfb2f1a052831daffa8a11061
Dmitriy Bushev <bushevdv@gmail.com>
2012-04-09 22:57:39 +0400

    Added interaction with cite menu

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,6 @@ import play.api.mvc._
 object Application extends Controller with ScalateEngine
     with Upload with Download {
   def index = Action {
-    Ok(render("views/index.jade"))
+    Ok(render("views/index.jade", "menu" -> Set("Home")))
   }
 }
