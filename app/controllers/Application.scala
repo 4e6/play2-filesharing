a6ddd312a9212063cf609d88df03d1264fd555ad
Dmitriy Bushev <bushevdv@gmail.com>
2012-04-13 08:16:00 +0400

    Added search page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,11 +6,17 @@ import lib.Helpers._
 
 object Application extends Controller with ScalateEngine
   with Upload with Download {
+
   def index = Action {
     val params = Map(
       "menu" -> Set("Home"),
       "filesizeLimit" -> lib.Config.filesizeLimit.toString,
       "readableFilesize" -> readableSize(lib.Config.filesizeLimit))
     Ok(render("views/index.jade", params))
+  }
+
+  def search = Action {
+    val params = Map("menu" -> Set("Search"))
+    Ok(render("views/search.jade", params))
   }
 }
