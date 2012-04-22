683b5a87c38172d4575c24e5bd40bc2b6991c42c
Dmitriy Bushev <bushevdv@gmail.com>
2012-04-22 22:40:09 +0400

    Move cite url to config

--- app/lib/Config.scala
+++ app/lib/Config.scala
@@ -9,6 +9,7 @@ import Helpers._
 object Config {
   val config = play.api.Play.current.configuration
 
+  lazy val appUrl = config.getString("app.url") | "http://localhost"
   lazy val logStatements_? = config.getBoolean("db.default.logStatements") | false
   lazy val storagePath = config.getString("storage.path") | "files"
   lazy val storageTime = config.getMilliseconds("storage.time") | 15.minutes.toMillis
